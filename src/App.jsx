69647afb841d38dc575e1b00a59b18d38cfa555f
Shreya Mathur <smathur245@gmail.com>
2025-11-24 10:32:51 +0530

    added login form validation

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import UserManagement from './pages/Dashboard/UserManagement/UserManagement'
 import Onboarding from './pages/Dashboard/Onboarding'
 import CostExplorer from './pages/Dashboard/CostExplorer'
 import AwsServices from './pages/Dashboard/AwsServices'
+import CreateUser from './pages/Dashboard/UserManagement/CreateUser'
 
 function App() {
 
@@ -17,7 +18,7 @@ function App() {
         <Route element={<ProtectedRoute />}>
           <Route path='/dashboard' element={<DashboardLayout />}>
             <Route path='user-management' element={<UserManagement />}>
-              <Route path='create' />
+              <Route path='create' element={<CreateUser />} />
             </Route>
             <Route path='onboarding' element={<Onboarding />}/>
             <Route path='cost-explorer' element={<CostExplorer />}/>
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -9,12 +9,43 @@ const Login = () => {
         email: "",
         password: ""
     });
+    const [errors, setErrors] = useState({
+        email: "",
+        password: ""
+    });
+    const [authError, setAuthError] = useState("");
 
 
     const handleChange = (e) => {
         setAuthData({...authData, [e.target.name]: e.target.value});
+        validateField(e.target.name, e.target.value);
         
     }
+
+    const validateField = (name, value) => {
+        let errorMessage = "";
+
+        if (name === "email") {
+            if (!value) {
+                errorMessage = "Email is required";
+            } else if (!/\S+@\S+\.\S+/.test(value)) {
+                errorMessage = "Email is invalid";
+            }
+        }
+
+        if (name === "password") {
+            if (!value) {
+                errorMessage = "Password is required";
+            } else if (value.length < 2) {
+                errorMessage = "Password must be at least 6 characters long";
+            }
+        }
+
+        setErrors({
+            ...errors,
+            [name]: errorMessage
+        });
+    };
 
     const authenticateUser = () => {
         console.log("Authenticating...");
@@ -22,34 +53,43 @@ const Login = () => {
         return user ? true : false;
     }
 
-    const handleSubmit = () => {
+    const handleSubmit = (e) => {
+        e.preventDefault();
         console.log(authData);
+        if (errors.email || errors.password || !authData.email || !authData.password) {
+            setAuthError("Please fix errors before submitting.");
+            return;
+        }
+
         if(authenticateUser()){
             localStorage.setItem("authData", JSON.stringify(authData));
             console.log("User logged in");
             navigate("/dashboard");
             
         }else{
-            console.log("Invalid username or password");
+            setAuthError("Invalid username or password");
         }
     }
 
   return (
     <div className=' w-screen h-screen flex justify-center items-center'>
-        <div className=' w-[28%] flex flex-col justify-start items-center'>
+        <form onSubmit={handleSubmit} className=' w-[28%] flex flex-col justify-start items-center'>
             <img className='w-[50%] mb-9' src="/logo.svg" alt="logo" />
-            <div className='w-full mb-9 flex flex-col justify-center items-start'>
+            <div className='w-full mb-2 flex flex-col justify-center items-start'>
                 <label className='mb-3 text-gray-800' htmlFor="email">Email</label>
-                <input value={authData.email} onChange={(e) => handleChange(e)} className='w-full border border-gray-600 rounded-md text-black py-3 px-4 font-bold placeholder:text-gray-500 ' type="text" name="email" id="email" placeholder='Enter email' />
+                <input value={authData.email} onChange={(e) => handleChange(e)} className='w-full border border-gray-600 rounded-md text-black py-3 px-4 font-bold placeholder:text-gray-500 ' type='email' name="email" id="email" placeholder='Enter email' />
             </div>
-            <div className='w-full mb-5 flex flex-col justify-center items-start'>
+            <div className='text-red-600 text-sm mb-8 w-full text-left'>{errors.email}</div>
+            <div className='w-full mb-2 flex flex-col justify-center items-start'>
                 <label className='mb-3 text-gray-800' htmlFor="password">Password</label>
                 <input value={authData.password} onChange={(e) => handleChange(e)} className='w-full border border-gray-600 rounded-md text-black py-3 px-4 font-bold placeholder:text-gray-500 ' type="password" name="password" id="password" placeholder='Enter password' />
             </div>
+            <div className='text-red-600 text-sm mb-5 w-full text-left'>{errors.password}</div>
             
             <Link className=' text-[#4398d7] mb-9 w-full text-right font-bold'>Forgot Password?</Link>
-            <button onClick={handleSubmit} className='w-full cursor-pointer py-3 rounded-md text-white font-bold bg-[#4398d7]'>LOGIN</button>
-        </div>
+            <div className='text-red-600 text-sm mb-2 w-full text-center'>{authError}</div>
+            <button type='submit' className='w-full cursor-pointer py-3 rounded-md text-white font-bold bg-[#4398d7]'>LOGIN</button>
+        </form>
         <LoginFooter />
     </div>
   )
